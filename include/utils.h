f0d551a7ca01cfed69f3e71a75aaf8175432378d
Nicholas John Koch <koch@njk-it.de>
2025-12-04 11:43:38 +0100

    Added some useful functions for the checksum stuff

--- include/utils.h
+++ include/utils.h
@@ -5,7 +5,9 @@
 
 #define HTML_BUFFER_SIZE (256 + 128)
 
-uint8_t calcChecksum(char* sentence);
+uint32_t hexStr2Int(char str[]);
+uint8_t checkChecksum(const char* nmea_line);
+uint8_t calcChecksum(const char* sentence);
 
 #ifdef WEB_GUI
 void str_replace(char* src, char* oldchars, char* newchars);
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,11 +1,12 @@
 #include "utils.h"
 
 #include <string.h>
+#include <stdlib.h>
 
 /**
  *
  */
-uint8_t calcChecksum(char* sentence) {
+uint8_t calcChecksum(const char* sentence) {
     // We calculate between $ and *
     uint8_t xsum = 0;
     const char* ptr = strchr(sentence, '*');
@@ -16,6 +17,32 @@ uint8_t calcChecksum(char* sentence) {
         }
     }
     return xsum;
+}
+
+uint32_t hexStr2Int(char str[]) { return (uint32_t)strtoul(str, 0, 16); }
+
+uint8_t checkChecksum(const char* nmea_line) {
+    if (nmea_line[0] != '$' && nmea_line[0] != '!') {
+        return 0;
+    }
+
+    uint8_t cs = calcChecksum(nmea_line);
+
+    // Get the checksum characters
+    char t[3];
+    const char* ptr = strchr(nmea_line, '*');
+    if (ptr) {
+        int16_t i = ptr - nmea_line;
+        t[0] = nmea_line[i + 1];
+        t[1] = nmea_line[i + 2];
+        t[2] = '\0';
+        if (cs != hexStr2Int(t)) {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+    return 1;
 }
 
 #ifdef WEB_GUI
