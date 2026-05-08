c539961e67285917c96fd0806d594831db72114f
Nicholas John Koch <koch@njk-it.de>
2026-05-08 11:21:38 +0200

    Did not like the default upper comma character so adapted one to fit the other.

---
+++ include/custom_chars.h
@@ -0,0 +1,7 @@
+#ifndef CUSTOM_CHARS_H_
+#define CUSTOM_CHARS_H_
+#include <stdint.h>
+
+extern const uint8_t upper_min_comma[];
+
+#endif
---
+++ src/custom_chars.cpp
@@ -0,0 +1,14 @@
+#include "custom_chars.h"
+
+#include <inttypes.h>
+
+const uint8_t upper_min_comma[] = {
+    0b01000,
+    0b01000,
+    0b01000,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000
+};
